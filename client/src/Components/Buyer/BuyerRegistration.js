20217a5c7f860c91ee3b2c82c91034d157dd7ced
Rajiramakrishnan <rajirajeswari50@gmail.com>
2025-02-04 14:16:37 +0530

    folder structure changed

---
+++ client/src/Components/Buyer/BuyerRegistration.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "../../Assets/style/BuyerRegistration.css"
+import 
+function BuyerRegistration() {
+  return (
+    <div>BuyerRegistration</div>
+  )
+}
+
+export default BuyerRegistration
